1bef3b2f4d99018b14866123dc696925aa81c416
Kim Jangwook <kim.jangwook.1029@gmail.com>
2021-05-30 00:39:12 +0900

    左上ウェーブメインビジュアル

---
+++ js/wave_background.js
@@ -0,0 +1,21 @@
+$('.svg-path1').wavify({
+    height: 60,
+    bones: 3,
+    amplitude: 40,
+    color: 'rgba(16, 148, 230, .5)',
+    speed: .25
+});
+$('.svg-path2').wavify({
+    height: 60,
+    bones: 3,
+    amplitude: 40,
+    color: 'rgba(230, 234, 240, .5)',
+    speed: .3
+});
+$('.svg-path3').wavify({
+    height: 60,
+    bones: 3,
+    amplitude35: 40,
+    color: 'rgba(165, 176, 234, .5)',
+    speed: .4
+});
